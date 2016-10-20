55bcfa4da0381e82d9a4257a385447b13d408502
Jannis Gebauer <ja.geb@me.com>
2016-10-20 17:32:43 +0200

    removed bogus test

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -20,21 +20,4 @@ from safety import cli
 
 
 class TestSafety(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_000_something(self):
-        pass
-
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'safety.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    pass

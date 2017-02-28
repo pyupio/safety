a66514fe6924b605ebd5fbb6072dc4dd6667b080
Jannis Gebauer <ja.geb@me.com>
2017-02-28 16:46:04 +0100

    remove the s3 mirrors as they are no longer present

--- safety/constants.py
+++ safety/constants.py
@@ -2,10 +2,6 @@
 
 DATABASE_MIRRORS = [
     "https://raw.githubusercontent.com/pyupio/safety-db/master/data/",
-    # S3 mirrors
-    "https://s3.eu-central-1.amazonaws.com/safety-db-mirror-1/data/",
-    "https://s3-us-west-2.amazonaws.com/safety-db-mirror-2/data/",
-    "https://s3.amazonaws.com/safety-db-mirror-3/data/"
 ]
 REQUEST_TIMEOUT = 5
 

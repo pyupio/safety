f37f38d32de01abf61674b34cbf8f8e40aab7859
Jannis Gebauer <ja.geb@me.com>
2017-03-21 17:13:16 +0100

    add basic report for terminals <80, closes #43

--- safety/formatter.py
+++ safety/formatter.py
@@ -1,60 +1,111 @@
 # -*- coding: utf-8 -*-
-REPORT_BANNER = """
-╒══════════════════════════════════════════════════════════════════════════════╕
-│                                                                              │
-│                               /$$$$$$            /$$                         │
-│                              /$$__  $$          | $$                         │
-│           /$$$$$$$  /$$$$$$ | $$  \__//$$$$$$  /$$$$$$   /$$   /$$           │
-│          /$$_____/ |____  $$| $$$$   /$$__  $$|_  $$_/  | $$  | $$           │
-│         |  $$$$$$   /$$$$$$$| $$_/  | $$$$$$$$  | $$    | $$  | $$           │
-│          \____  $$ /$$__  $$| $$    | $$_____/  | $$ /$$| $$  | $$           │
-│          /$$$$$$$/|  $$$$$$$| $$    |  $$$$$$$  |  $$$$/|  $$$$$$$           │
-│         |_______/  \_______/|__/     \_______/   \___/   \____  $$           │
-│                                                          /$$  | $$           │
-│                                                         |  $$$$$$/           │
-│  by pyup.io                                              \______/            │
-│                                                                              │
-╞══════════════════════════════════════════════════════════════════════════════╡
-""".strip()
+try:
+    from shutil import get_terminal_size
+except ImportError:
+    # fallback for python < 3
+    import subprocess
+    from collections import namedtuple
 
-TABLE_HEADING = """
-╞════════════════════════════════╤═══════════════╤═════════════════════════════╡
-│ package                        │ installed     │ affected                    │
-╞════════════════════════════════╧═══════════════╧═════════════════════════════╡
-""".strip()
+    def get_terminal_size():
+        size = namedtuple("_", "rows", "columns")
+        try:
+            rows, columns = subprocess.check_output(['stty', 'size']).split()
+            return size(rows=int(rows), columns=int(columns))
+        except (ValueError, FileNotFoundError):
+            return size(rows=0, columns=0)
 
-TABLE_FOOTER = """
-╘════════════════════════════════╧═══════════════╧═════════════════════════════╛
-""".strip()
 
-TABLE_BREAK = """
-╞════════════════════════════════╡═══════════════╡═════════════════════════════╡
-""".strip()
+class SheetReport(object):
+    REPORT_BANNER = """
+    ╒══════════════════════════════════════════════════════════════════════════════╕
+    │                                                                              │
+    │                               /$$$$$$            /$$                         │
+    │                              /$$__  $$          | $$                         │
+    │           /$$$$$$$  /$$$$$$ | $$  \__//$$$$$$  /$$$$$$   /$$   /$$           │
+    │          /$$_____/ |____  $$| $$$$   /$$__  $$|_  $$_/  | $$  | $$           │
+    │         |  $$$$$$   /$$$$$$$| $$_/  | $$$$$$$$  | $$    | $$  | $$           │
+    │          \____  $$ /$$__  $$| $$    | $$_____/  | $$ /$$| $$  | $$           │
+    │          /$$$$$$$/|  $$$$$$$| $$    |  $$$$$$$  |  $$$$/|  $$$$$$$           │
+    │         |_______/  \_______/|__/     \_______/   \___/   \____  $$           │
+    │                                                          /$$  | $$           │
+    │                                                         |  $$$$$$/           │
+    │  by pyup.io                                              \______/            │
+    │                                                                              │
+    ╞══════════════════════════════════════════════════════════════════════════════╡
+    """.strip()
 
-REPORT_HEADING = """
-│ REPORT                                                                       │
-""".strip()
+    TABLE_HEADING = """
+    ╞════════════════════════════════╤═══════════════╤═════════════════════════════╡
+    │ package                        │ installed     │ affected                    │
+    ╞════════════════════════════════╧═══════════════╧═════════════════════════════╡
+    """.strip()
 
-REPORT_SECTION = """
-╞══════════════════════════════════════════════════════════════════════════════╡
-""".strip()
+    TABLE_FOOTER = """
+    ╘════════════════════════════════╧═══════════════╧═════════════════════════════╛
+    """.strip()
 
-REPORT_FOOTER = """
-╘══════════════════════════════════════════════════════════════════════════════╛
-""".strip()
+    TABLE_BREAK = """
+    ╞════════════════════════════════╡═══════════════╡═════════════════════════════╡
+    """.strip()
+
+    REPORT_HEADING = """
+    │ REPORT                                                                       │
+    """.strip()
+
+    REPORT_SECTION = """
+    ╞══════════════════════════════════════════════════════════════════════════════╡
+    """.strip()
+
+    REPORT_FOOTER = """
+    ╘══════════════════════════════════════════════════════════════════════════════╛
+    """.strip()
+
+    @staticmethod
+    def render(vulns):
+        if vulns:
+            table = []
+            for vuln in vulns:
+                table.append("│ {:30} │ {:13} │ {:27} │".format(
+                    vuln.name[:30],
+                    vuln.version[:13],
+                    vuln.spec[:27]
+                ))
+            table = "\n".join(table)
+            return "\n".join(
+                [SheetReport.REPORT_BANNER, SheetReport.REPORT_HEADING, SheetReport.TABLE_HEADING,
+                 table, SheetReport.TABLE_FOOTER]
+            )
+        else:
+            content = "│ {:76} │".format("No known security vulnerabilities found.")
+            return "\n".join(
+                [SheetReport.REPORT_BANNER, SheetReport.REPORT_HEADING, SheetReport.REPORT_SECTION,
+                 content, SheetReport.REPORT_FOOTER]
+            )
+
+
+class BasicReport(object):
+    """Basic report, intented to be used for terminals with < 80 columns"""
+
+    @staticmethod
+    def render(vulns):
+        table = ["safety report", "---"]
+        if vulns:
+
+            for vuln in vulns:
+                table.append("-> {}, installed {}, affected {}".format(
+                    vuln.name,
+                    vuln.version[:13],
+                    vuln.spec[:27]
+                ))
+        else:
+            table.append("No known security vulnerabilities found.")
+        return "\n".join(
+            table
+        )
 
 
 def report(vulns):
-    if vulns:
-        table = []
-        for vuln in vulns:
-            table.append("│ {:30} │ {:13} │ {:27} │".format(
-                vuln.name[:30],
-                vuln.version[:13],
-                vuln.spec[:27]
-            ))
-        table = "\n".join(table)
-        return "\n".join([REPORT_BANNER, REPORT_HEADING, TABLE_HEADING, table, TABLE_FOOTER])
-    else:
-        report = "│ {:76} │".format("No known security vulnerabilities found.")
-        return "\n".join([REPORT_BANNER, REPORT_HEADING, REPORT_SECTION, report, REPORT_FOOTER])
+    size = get_terminal_size()
+    if size.columns >= 80:
+        return SheetReport.render(vulns)
+    return BasicReport.render(vulns)

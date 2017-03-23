e9c24b32b9be7ab2a38f4134a541e9bad44421dd
Jannis Gebauer <ja.geb@me.com>
2017-03-23 16:37:29 +0100

    added basic safety.check test

--- tests/test_safety.py
+++ tests/test_safety.py
@@ -17,6 +17,12 @@ from click.testing import CliRunner
 from safety import safety
 from safety import cli
 from safety import formatter
+from safety import util
+import os
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class TestSafetyCLI(unittest.TestCase):
@@ -38,3 +44,21 @@ class TestFormatter(unittest.TestCase):
             formatter.get_terminal_size()
         except TypeError as e:
             self.fail(e)
+
+
+class TestSafety(unittest.TestCase):
+
+    def test_check(self):
+        reqs = StringIO("Django==1.8.1")
+        packages = util.read_requirements(reqs)
+
+        vulns = safety.check(
+            packages=packages,
+            db_mirror=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "test_db"
+            ),
+            cached=False,
+            key=False
+        )
+        self.assertEqual(len(vulns), 2)

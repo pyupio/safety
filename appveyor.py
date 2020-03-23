e5d37fdfaf820907063642135eb4d21e779d950c
Rafael Pivato <rpivato@gmail.com>
2020-03-23 15:15:09 -0300

    Do not use requirements_dev.txt

--- appveyor.py
+++ appveyor.py
@@ -1,5 +1,8 @@
-"""
-This file is used to build and distribute the safety binary on appveyor, take a look at the corresponding appveyor.yml.
+"""AppVeyor Build
+
+This file is used to build and distribute the safety binary on appveyor. Take
+a look at the corresponding appveyor.yml as well.
+
 """
 import os
 import sys
@@ -8,11 +11,15 @@ import subprocess
 
 class environment:
 
+    WIN = "win"
+    LINUX = "linux"
+    MACOS = "macos"
+
     def __init__(self):
         os_mapping = {
-            "Visual Studio 2019": "win",
-            "Ubuntu": "linux",
-            "macOS": "macos"
+            "Visual Studio 2019": self.WIN,
+            "Ubuntu": self.LINUX,
+            "macOS": self.MACOS
         }
         self.os = os_mapping[os.getenv("APPVEYOR_BUILD_WORKER_IMAGE")]
 
@@ -21,35 +28,32 @@ class environment:
         for arch, python in self.PYTHON_BINARIES[self.os].items():
             yield arch, python
 
-    WIN = "win"
-    LINUX = "linux"
-    MACOS = "macos"
-
     PYTHON_BINARIES = {
         WIN: {
             64: "C:\\Python38-x64\\python.exe",
             32: "C:\\Python38\\python.exe",
-
         },
         LINUX: {
-            # order is important. if the 32 bit release gets built first,
+            # Order is important. If the 32 bit release gets built first,
             # you'll run into permission problems due to docker clobbering
-            # up the current working directory
+            # up the current working directory.
             64: "python",
+
             # 32 bit linux is built using docker
             32: f"docker run -t -v {os.getcwd()}:/app 32-bit-linux python3",
-
         },
         MACOS: {
-            # on macOS the binary is built using Python 3.7 (installed via Homebrew), because
+            # On macOS the binary is built using Python 3.7 (Homebrew), because
             # the shipped Python lacks libraries PyInstaller needs.
             64: "/usr/local/bin/python3",
         }
     }
 
     def run(self, command):
-        """
-        Runs the given command via subprocess.check_output, exits with -1 if the command wasn't successfull.
+        """Runs the given command via subprocess.check_output.
+
+        Exits with -1 if the command wasn't successfull.
+
         """
         try:
             print(f"RUNNING: {command}")
@@ -72,22 +76,29 @@ class environment:
             self.run("docker build -t 32-bit-linux -f Dockerfilei386 .")
 
         for arch, python in self.python:
-            self.run(f"{python} -m pip install -r requirements_dev.txt")
+            self.run(f"{python} --no-cache-dir -m pip install pytest")
+            self.run(f"{python} --no-cache-dir -m pip install -e .")
 
     def dist(self):
-        """
-        Runs Pyinstaller producing a single file binary for every available arch for the current platform.
-        """
+        """Runs Pyinstaller producing a binary for every platform arch."""
         for arch, python in self.python:
 
-            # build the binary
+            # Build the binary
             build_path = os.path.join("dist", f"safety-{arch}")
-            self.run(f"{python} -m PyInstaller safety.spec --distpath {build_path}")
+            self.run(f"{python} -m PyInstaller safety.spec"
+                     f" --distpath {build_path}")
 
-            # there seems to be no way to tell pyinstaller the name of the actual binary
-            # this leads to problems with appveyors artifact collector because every binary is named the same
-            # move them around so they can be picked up correctly
-            artifact_path = os.path.join(os.getcwd(), "dist", f"safety-{self.os}-{'i686' if arch == 32 else 'x86_64'}")
+            # There seems to be no way to tell pyinstaller the binary name.
+            # This leads to problems with appveyors artifact collector because
+            # every binary is named the same.
+            #
+            # Move them around so they can be picked up correctly
+            #
+            artifact_path = os.path.join(
+                os.getcwd(),
+                "dist",
+                f"safety-{self.os}-{'i686' if arch == 32 else 'x86_64'}"
+            )
             binary_path = os.path.join(os.getcwd(), build_path, "safety")
             if self.os == self.WIN:
                 self.run(f"move {binary_path}.exe {artifact_path}.exe")
@@ -99,7 +110,7 @@ class environment:
         Runs tests for every available arch on the current platform.
         """
         for arch, python in self.python:
-            self.run(f"{python} setup.py test")
+            self.run(f"{python} -m pytest")
 
 
 if __name__ == "__main__":
@@ -109,6 +120,7 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     env = environment()
-    # runs the command in sys.argv[1] (install|test|dist)
+
+    # Runs the command in sys.argv[1] (install|test|dist)
     getattr(env, sys.argv[1])()
     sys.exit(0)

b99f435616196db4f64e94f0f4ee8d79909ba5e8
Christopher Goes <ghostofgoes@gmail.com>
2020-03-30 19:10:57 -0600

    Requested changes

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     zip_safe=False,
     keywords='safety',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
@@ -53,6 +53,5 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-    ],
-    test_suite='tests'
+    ]
 )

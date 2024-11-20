87d9a6612013786fa7a802a35edb54847f76923d
Dylan Pulver <dylanryanpulver@gmail.com>
2024-11-20 17:10:48 -0500

    checking vulns

--- cli_isolated/main.py
+++ cli_isolated/main.py
@@ -2,12 +2,17 @@ from pathlib import Path
 from safety_schemas.models import FileType
 
 from read_dependency_files import read_dependency_files
+from vulnerability_checker import check_vulnerabilities
+from rich.console import Console
 
+console = Console()
 
 file_path = Path("/Users/dylanpulver/Repos/pyup/safety/test_requirements.txt")
 file_type = FileType.REQUIREMENTS_TXT
 
 # Call the function and consume the generator
+# Process and check vulnerabilities
 for path, inspectable_file in read_dependency_files([file_path], [file_type]):
-    print(f"Processed file: {path}")
-    print(f"Inspectable File: {inspectable_file}")
+    console.print(f"Processed file: {path}")
+    file_model = check_vulnerabilities(path, inspectable_file, console)
+    console.print(f"File Model: {file_model}")
